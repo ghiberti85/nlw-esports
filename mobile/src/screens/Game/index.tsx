233ddb7a85bdc94630c7a8830edb923bf06a53e6
ghiberti85 <ghiberti85@gmail.com>
2022-09-18 15:38:51 -0300

    feat: finish mobile interface

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -10,10 +10,12 @@ import { Heading } from '../../components/Heading';
 import { THEME } from '../../theme';
 import React, { useEffect, useState } from 'react';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { DuoMatch } from '../../components/DuoMatch';
 
 export function Game() {
 
   const [duos, setDuos] =  useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -21,6 +23,12 @@ export function Game() {
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.0.21:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord));
   }
 
   useEffect (() => {
@@ -63,7 +71,9 @@ export function Game() {
           data={duos}
           keyExtractor={ item => item.id }
           renderItem={({item}) => (
-            <DuoCard data={item} onConnect ={() => {}}/>
+            <DuoCard 
+              data={item} 
+              onConnect ={() => getDiscordUser(item.id)}/>
           )}
           horizontal
           style={styles.containerList}
@@ -75,6 +85,12 @@ export function Game() {
               Ainda não há anúncios publicados.
             </Text>
           )}
+        />
+
+        <DuoMatch 
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
 
       </SafeAreaView>
